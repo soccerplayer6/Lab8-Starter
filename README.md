195b3a898d271a52babece2269f8546c7bbd57a5
soccerplayer6 <32963392+soccerplayer6@users.noreply.github.com>
2024-05-22 21:37:37 -0700

    almost done

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Lab8-Starter
+Graceful degradation and serviceWorkers are related because serviceWorkers are used to limit network requests and make your application usable offline. Graceful degradation has to do with your application being used on different devices and you building in fallbacks in case a device without internet connection is using your application. 
--- sw.js
+++ sw.js
@@ -9,8 +9,15 @@ self.addEventListener('install', function (event) {
     caches.open(CACHE_NAME).then(function (cache) {
       // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
       //            added to the cache when the ServiceWorker is installed
-      
-      return cache.addAll([]);
+      const RECIPE_URLS = [
+        'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/3_moms-cornbread-stuffing.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json',
+    ];
+      return cache.addAll(RECIPE_URLS);
     })
   );
 });
@@ -35,7 +42,26 @@ self.addEventListener('fetch', function (event) {
   /*******************************/
   // B7. TODO - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
+  self.addEventListener('install', (event) => {
+    event.waitUntil(caches.open(CACHE_NAME));
+  });
   // B8. TODO - If the request is in the cache, return with the cached version.
-  //            Otherwise fetch the resource, add it to the cache, and return
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse; // If the request is in the cache, return it
+      }
+
+      //            Otherwise fetch the resource, add it to the cache, and return
   //            network response.
+
+      return fetch(event.request).then((networkResponse) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, networkResponse.clone()); // Add the network response to the cache
+          return networkResponse; // Return the network response
+        });
+      });
+    })
+  );
+
 });
