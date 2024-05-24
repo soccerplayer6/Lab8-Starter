7c90508c2a54cc42041783281b10a419015370c3
soccerplayer6 <32963392+soccerplayer6@users.noreply.github.com>
2024-05-23 23:17:25 -0700

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -50,7 +50,7 @@ function initializeServiceWorker() {
       window.addEventListener('load', async () => {
         
         try { 
-          const registration = await navigator.serviceWorker.register("/Lab8-Starter/sw.js", {
+          const registration = await navigator.serviceWorker.register("/sw.js", {
             scope: "/",
           });
           if (registration.installing) {
